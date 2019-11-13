a8c24c94d530d8f3b34b83b614169461f3370705
daniel <daniel.sc.crawford@gmail.com>
2019-11-13 13:58:17 -0600

    Working on miniproject 5 for cs6313

--- Senior/Fall_2019/CS6313/Miniproject_5/q2.R
+++ Senior/Fall_2019/CS6313/Miniproject_5/q2.R
@@ -3,33 +3,50 @@ rm(list = ls())
 options(digits=5)
 
 in_confidence_interval = function(mean, CI) {
-  return(mean >= CI[1] && mean <= CI[2])
+  return(mean >= CI[1] & mean <= CI[2])
 }
 
-coverage_probability = function(lambda, CI) {
-  return(sum(replicate(100, in_confidence_interval(mean(rexp(10, lambda)), CI))) / 100)
+capture_percentage = function(B, t) {
+  CI = percentile_confidence_interval(B$t)
+  return(sum(in_confidence_interval(t, CI)) / length(B$t))
 }
 
-coverage_probability_percentile = function(X, indices, lambda) {
-  D  = sort(X[indices])
-  CI = c(D[alpha / 2 * length(D) + 1], D[(1 - alpha / 2) * length(D) + 1])
-  return(coverage_probability(lambda, CI))
+generate_sample = function(D, par) {
+  return(rexp(par[1], par[2]))
 }
 
-coverage_probability_large_sample = function(X, indices, lambda) {
-  D  = X[indices]
-  CI = mean(D) + c(-1, 1) * qnorm(0.975) * sd(D) / sqrt(length(D))
-  return(coverage_probability(lambda, CI))
+percentile_confidence_interval = function(D) {
+  est = sort(D)
+  n = length(est)
+  alpha = 0.05
+  return(c(est[alpha / 2 * n + 1], est[(1 - alpha/2) * n + 1]))
 }
 
-D = expand.grid(c(5, 10, 30, 100),c(0.01, 0.1, 1, 10))
+run_test = function(n, lambda, MC) {
+  tMean = 1 / lambda
+  B = replicate(5000, boot(data = rexp(n, lambda), 
+                           statistic = mean, R=b, sim = "parametric", 
+                           ran.gen = generate_sample, mle = c(n, lambda)))
+  
+}
+
+df = expand.grid(n=c(5, 10, 30, 100), lambda=c(0.01, 0.1, 1, 10))
 alpha = 0.05
 b     = 999
+MC = replicate(999, mean(rexp(100, 1/100)))
 
-r = apply(D, 1, FUN=function(x) boot(rexp(x[1], x[2]), coverage_probability_percentile, 
-                                     lambda = as.numeric(x[2]), R=999))
+results = apply(df, 1, function(x) run_test(as.numeric(x[1]), as.numeric(x[2])))
 
-t = apply(D, 1, FUN=function(x) boot(rexp(x[1], x[2]), coverage_probability_large_sample, 
-                                     lambda = as.numeric(x[2]), R=999)$t0)
+n = 5
+lambda = 0.01
+B = replicate(5000, boot(data = rexp(n, lambda), 
+                         statistic = mean, R=b, sim = "parametric", 
+                         ran.gen = generate_sample, mle = c(n, lambda)))
 
-x = D[1,]
+#apply(df, 1, FUN=function(x) boot(unlist(df$sample),
+#                                  mean, lambda = as.numeric(df[2]), R=b))
+
+# What to do:
+# Generate 5000 means
+# Check from each generated mean if it is captured by the bootstrap interval
+
