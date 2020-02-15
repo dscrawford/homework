395c6772dd41b58c9f7c0633078381b7576fbf42
Daniel Crawford <daniel.sc.crawford@gmail.com>
2020-02-15 14:40:18 -0600

    Working on CS6347 homework 2

--- Senior/Spring_2020/CS6347/Homework2/varElim.py
+++ Senior/Spring_2020/CS6347/Homework2/varElim.py
@@ -2,15 +2,60 @@ import numpy as np
 import pandas as pd
 import regex as re
 
-s = re.sub('^c.*\n?', '', open("network.uai", "r").read(), flags=re.MULTILINE)
-s = [l for l in s.split('\n') if l]
+
+class GraphicalModel:
+    networkType = ""
+    varN = 0
+    varCardinalities = np.array([])
+    cliques = 0
+    cliqueScopes = np.array([])
+    functionTables = []
+    evidence = np.array([])
+
+    def __init__(self, uaiFile: str):
+        self.parseUAI(uaiFile + ".uai")
+        self.parseUAIEvidence(uaiFile + ".uai.evid")
+
+    def instantiateEvidence(self):
+        evidenceVars = list([i[0] for i in self.evidence])
+        evidenceVals = list([i[1] for i in self.evidence])
+        card         = self.varCardinalities
+        for csi, cs in enumerate(self.cliqueScopes):
+            M = np.reshape(self.functionTables[csi], self.varCardinalities[self.cliqueScopes[csi]])
 
 
-networkType = s.pop(0)
-varN        = int(s.pop(0))
-varCard     = [int(d) for d in s.pop(0).split(' ')]
-cliques     = int(s.pop(0))
+    def parseUAIEvidence(self, evidenceFile: str):
+        s = [t for t in open(evidenceFile, "r").read().split(' ') if t]
+        observedVariables = int(s.pop(0))
+        self.evidence = [(int(s[2*i]),int(s[2*i + 1])) for i in range(0, observedVariables)]
 
-cliqueScopes = [[int(d) for d in s[i].split(' ') if d] for i in range(4,cliqueI + 1)]
+    def parseUAI(self, uaiFile: str):
+        s = re.sub('^c.*\n?', '', open(uaiFile, "r").read(), flags=re.MULTILINE)
+        s = [l for l in s.split('\n') if l]
+        # Below parses the UAI file
+        while (len(s) != 0):
+            data = s.pop(0)
+            if (data.upper() in ['MARKOV', 'BAYES']):
+                self.networkType = data
+                self.varN = int(s.pop(0))
+                self.varCardinalities = np.array([int(d) for d in s.pop(0).split(' ') if d])
+                self.cliques = int(s.pop(0))
+                cliqueScopes = []
+                while (True):
+                    cs = s.pop(0)
+                    cliqueScopes += [[int(d) for d in cs.split(' ') if d][1:]]
+                    if (len([t for t in s[0] if t]) == 1):
+                        break
+                self.cliqueScopes = np.array(cliqueScopes)
+            if (data.isdigit()):
+                entriesN = int(data)
+                entries = []
+                entriesAdded = 0
+                while (entriesAdded != entriesN):
+                    newEntries = [float(d) for d in s.pop(0).split(' ') if d]
+                    entriesAdded += len(newEntries)
+                    entries += newEntries
+                self.functionTables += [np.array(entries)]
 
-functionTables = []
+
+network = GraphicalModel("network")
