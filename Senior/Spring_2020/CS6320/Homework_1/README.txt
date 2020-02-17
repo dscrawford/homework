7a67965fd2cbd1fb654995d6720ca49a6be766d4
Daniel Crawford <daniel.sc.crawford@gmail.com>
2020-02-17 00:00:36 -0600

    Got some neat things working on CS6347 hw1 lmao

---
+++ Senior/Spring_2020/CS6320/Homework_1/README.txt
@@ -0,0 +1,15 @@
+# Setup
+
+Please have train.txt and test.txt in the same directory as the .cc files.
+Then, type 'make' to compile everything.
+
+# Execution
+
+./bigram [train file name] [test file name] [0|1]
+EX:
+./bigram train.txt test.txt 1
+
+# Formatting
+
+To avoid formatting errors, disable any text wrap and the matrices will be aligned
+For example, in emacs type "M-x toggle-truncate-mode" to format the matrices correctly.
--- Senior/Spring_2020/CS6347/Homework2/varElim.py
+++ Senior/Spring_2020/CS6347/Homework2/varElim.py
@@ -6,28 +6,54 @@ import regex as re
 class GraphicalModel:
     networkType = ""
     varN = 0
-    varCardinalities = np.array([])
+    card = np.array([])
     cliques = 0
     cliqueScopes = np.array([])
     functionTables = []
+    stride = []
     evidence = np.array([])
 
     def __init__(self, uaiFile: str):
         self.parseUAI(uaiFile + ".uai")
         self.parseUAIEvidence(uaiFile + ".uai.evid")
 
-    def instantiateEvidence(self):
-        evidenceVars = list([i[0] for i in self.evidence])
-        evidenceVals = list([i[1] for i in self.evidence])
-        card         = self.varCardinalities
-        for csi, cs in enumerate(self.cliqueScopes):
-            M = np.reshape(self.functionTables[csi], self.varCardinalities[self.cliqueScopes[csi]])
+    def getIndex(self, assignment: np.array, stride: np.array):
+        return sum(assignment * stride)
 
+    def getAssignment(self, index: int, stride: int, card: int):
+        return index // stride % card
+
+    def factorProduct(self, f1, f2):
+        j = 0
+        k = 0
+        n = np.sum(self.card[list(set(self.cliqueScopes[f1]) & set(self.cliqueScopes[f2]))])
+        vn = len(self.cliqueScopes[f1]) + len(self.cliqueScopes[f2])
+        assignment = np.full(n, 0)
+        psi = np.full(n, 0)
+        for i in range(0, vn):
+            psi[i] = self.cliqueScopes[f1][j] * self.cliqueScopes[f2][k]
+            for l in range(0, n):
+                assignment[l] = assignment[l] + 1
+                if assignment[l] == self.card[l]:
+                    assignment[l] = 0
+                    j = j - (self.card[l] - 1) * (0 if len(self.stride) >= l else self.stride[l])
+                    k = k - (self.card[k] - 1) * (0 if len(self.stride) >= k else self.stride[k])
+                else:
+                    j = j + (0 if len(self.stride) >= l else self.stride[l])
+                    k = k + (0 if len(self.stride) >= k else self.stride[k])
+        return psi
+
+    def instantiateEvidence(self):
+        print("what")
+        # for i,(vars,table) in enumerate(self.functionTables):
+        # for var in vars:
+        # if (var == )
+        #    index = [j]
 
     def parseUAIEvidence(self, evidenceFile: str):
         s = [t for t in open(evidenceFile, "r").read().split(' ') if t]
         observedVariables = int(s.pop(0))
-        self.evidence = [(int(s[2*i]),int(s[2*i + 1])) for i in range(0, observedVariables)]
+        self.evidence = [(int(s[2 * i]), int(s[2 * i + 1])) for i in range(0, observedVariables)]
 
     def parseUAI(self, uaiFile: str):
         s = re.sub('^c.*\n?', '', open(uaiFile, "r").read(), flags=re.MULTILINE)
@@ -38,7 +64,7 @@ class GraphicalModel:
             if (data.upper() in ['MARKOV', 'BAYES']):
                 self.networkType = data
                 self.varN = int(s.pop(0))
-                self.varCardinalities = np.array([int(d) for d in s.pop(0).split(' ') if d])
+                self.card = np.array([int(d) for d in s.pop(0).split(' ') if d])
                 self.cliques = int(s.pop(0))
                 cliqueScopes = []
                 while (True):
@@ -56,6 +82,8 @@ class GraphicalModel:
                     entriesAdded += len(newEntries)
                     entries += newEntries
                 self.functionTables += [np.array(entries)]
+            self.stride = np.array([[np.product(self.card[cs[range(0, i)]]) for i, _ in enumerate(cs)]
+                                    for cs in self.cliqueScopes])
 
 
 network = GraphicalModel("network")
