2f94f0ccce6b4d13bf7c3cb72864283c831b8d5a
Daniel Crawford <daniel.sc.crawford@gmail.com>
2020-02-27 00:23:02 -0600

    Just gotta finish up some small details for cs6320 homework 2

--- Senior/Spring_2020/CS6320/Homework_2/CS6320_Homework2.py
+++ Senior/Spring_2020/CS6320/Homework_2/CS6320_Homework2.py
@@ -1,30 +1,37 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 from os import listdir
 from sys import argv
 from os.path import exists
+import argparse
 
 
-def reportPerformance(y_pred, test_labels, model_name):
-    print(model_name)
+def reportPerformance(y_pred, test_labels, description):
+    print(description)
     print('Accuracy:  ', accuracy_score(y_pred, test_labels), '\n',
           'Precision: ', precision_score(y_pred, test_labels), '\n',
           'Recall:    ', recall_score(y_pred, test_labels), '\n',
           'F-Score:   ', f1_score(y_pred, test_labels), '\n', sep='')
 
 
-if len(argv) != 7:
-    print('Error: Insufficient amount of arguments')
-    exit(1)
-train_dir = argv[1] + '/'
-test_dir = argv[2] + '/'
-representation = argv[3]
-classifier = argv[4]
-stop_words = argv[5]
-regularization = argv[6]
+parser = argparse.ArgumentParser(description='Arguments for training a basic word model')
+parser.add_argument('train_dir', metavar='train_Dir', type=str,
+                    help='Directory to training text files')
+parser.add_argument('test_dir', metavar='test_dir', type=str,
+                    help='Directory to testing text files')
+parser.add_argument('representation', metavar='representation', type=str, choices=['bow', 'tfidf'],
+                    help='Can either be \'bow\' or \'tfidf\'')
+parser.add_argument('classifier', metavar='classifier', type=str, choices=['regression', 'nbayes'],
+                    help='Can either be \'regression\' or \'nbayes\'')
+parser.add_argument('stop_words', metavar='stop_words', type=int, choices=[0, 1],
+                    help='Can either be \'0\' or \'1\'')
+parser.add_argument('--regularization', '-r', metavar='reg', type=str, default=None,
+                    choices=['no', 'l1', 'l2', ''], help='Can either be \'no\', \'l1\', \'l2\'')
+args = parser.parse_args()
+train_dir = args.train_dir + '/'
+test_dir = args.test_dir + '/'
+representation = args.representation
+classifier = args.classifier
+stop_words = args.stop_words
+regularization = args.regularization
 if not exists(train_dir):
     print('Error: train directory \'', train_dir,
           '\' does not exist.', sep='')
@@ -33,23 +40,15 @@ if not exists(test_dir):
     print('Error: test directory \'', test_dir,
           '\' does not exist.', sep='')
     exit(1)
-if representation not in ['bow', 'tfidf']:
-    print('Error: representation \'', representation,
-          '\' is not supported', sep='')
-    exit(1)
-if classifier not in ['nbayes', 'regression']:
-    print('Error: classifier \'', classifier,
-          '\' is not supported', sep='')
-    exit(1)
-if stop_words not in ['0', '1']:
-    print('Error: stop words must be 0 or 1')
-    exit(1)
-if regularization not in ['no', 'l1', 'l2']:
-    print('Error: regularization must be no, l1 or l2')
-    exit(1)
-stop_words = 'english' if stop_words == '1' else ''
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+
+stop_words = 'english' if stop_words == 1 else None
 C = 9e7 if regularization == 'no' else 1.0
-regularization = 'l2' if regularization == 'no' else regularization
+reg = 'l2' if regularization in ['no', None] else regularization
 train_labels = []
 train_files = []
 test_labels = []
@@ -67,10 +66,13 @@ for l in label_dir:
         test_files.append(test_dir + l + '/' + file)
 X_train = X.fit_transform(train_files)
 X_test = X.transform(test_files)
-Model = LogisticRegression(penalty=regularization, C=C, max_iter=1000) if classifier == 'regression' \
-    else MultinomialNB()
+Model = LogisticRegression(penalty=reg, C=C, max_iter=1000, solver='liblinear') \
+    if classifier == 'regression' else MultinomialNB()
 F = Model.fit(X_train, train_labels)
 
 classifierName = 'Logistic Regression' if classifier == 'regression' else 'Naive Bayes'
 representationName = 'CountVectorizer' if representation == 'bow' else 'TfidfVectorizer'
-reportPerformance(F.predict(X_test), test_labels, classifierName + ' with ' + representationName)
+stopWords = 'with stop words' if stop_words == 'english' else 'without stop words'
+regWords = '' if regularization in ['no', None] else 'with ' + reg + ' regularization'
+reportPerformance(F.predict(X_test), test_labels, classifierName + ' with ' + representationName + ' ' \
+                  + stopWords + ' ' + regWords)
--- Senior/Spring_2020/CS6320/Homework_2/runAll.py
+++ Senior/Spring_2020/CS6320/Homework_2/runAll.py
@@ -1,4 +1,12 @@
 import os
 
-train_dir = ['
-for 
+train_dir = 'aclImdb/train'
+test_dir = 'aclImdb/test'
+executable = 'CS6320_Homework2.py'
+for r in ['bow', 'tfidf']:
+    for s in ['0','1']:
+        os.system('python ' + executable + ' ' + train_dir + ' ' + test_dir + ' ' + r + ' nbayes ' + s)
+for r in ['bow', 'tfidf']:
+    for s in ['0','1']:
+        for reg in ['no', 'l1', 'l2']:
+            os.system('python ' + executable + ' ' + train_dir + ' ' + test_dir + ' ' + r + ' regression ' + s + ' -r ' + reg)
